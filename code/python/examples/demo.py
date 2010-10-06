ef892314491d0de80d60e048e0d5bccd54af5dd7
tmet <tmet@03f8628b-ed47-4a1f-bca9-bd5ac709a1f8>
2010-10-06 20:40:48 +0000

    cleanup.
    git-svn-id: file://localhost/tmp/svn2git/occi-wg/svn/trunk@166 03f8628b-ed47-4a1f-bca9-bd5ac709a1f8

--- code/python/examples/demo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import urllib2
-response = urllib2.urlopen('http://cloud.example.com/myvm')
-representation = response.read()
-metadata = response.info()
-print metadata['occi-compute-cores']
-
